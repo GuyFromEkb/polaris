278fe79e0b706bef327d0179cb6c9b0224c2c651
GuyFromEkb <guyFromEkb@example.com>
2026-03-15 21:40:26 +0500

    feat: добавил Firecrawl для URL scraping

---
+++ src/firecrawl/client.ts
@@ -0,0 +1,3 @@
+import Firecrawl from "@mendable/firecrawl-js"
+
+export const firecrawl = new Firecrawl({ apiKey: process.env.NEXT_PUBLIC_FIRECRAWL_API_KEY! })
--- src/ingest/functions.ts
+++ src/ingest/functions.ts
@@ -1,16 +1,56 @@
 import { google } from "@ai-sdk/google"
 import { generateText } from "ai"
 import { inngest } from "./client"
+import { firecrawl } from "~/firecrawl/client"
+
+const REGEX_URL =
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g
 
 export const unblock = inngest.createFunction(
   { id: "unblock" },
   { event: "test/unblock" },
-  async ({ event }) => {
-    const result = await generateText({
-      model: google("gemini-3.1-flash-lite-preview"),
-      prompt: event.data.message,
+  async ({ step, event }) => {
+    const prompt = event.data.message as string
+    const urls = [...new Set(prompt.match(REGEX_URL) ?? [])]
+
+    const context = urls.length
+      ? await step.run("get context from urls", async () => {
+          const scrapeResults = await Promise.allSettled(
+            urls.map((url) =>
+              firecrawl.scrape(url, {
+                formats: ["markdown"],
+              }),
+            ),
+          )
+
+          return scrapeResults.flatMap((result) => {
+            if (result.status !== "fulfilled") return []
+            if (!result.value?.markdown) return []
+
+            return [result.value.markdown]
+          })
+        })
+      : []
+
+    const finalPrompt = context.length
+      ? `
+Context from links:
+${context.join("\n\n---\n\n")}
+
+Question:
+${prompt.replace(REGEX_URL, "").trim()}
+`.trim()
+      : prompt
+
+    const result = await step.run("generate answer", async () => {
+      const generateData = await generateText({
+        model: google("gemini-3.1-flash-lite-preview"),
+        prompt: finalPrompt,
+      })
+
+      return generateData.text
     })
 
-    return { message: result.text }
+    return { message: result }
   },
 )
